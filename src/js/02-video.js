d0e356f9b84fa3f80f9eed038b904bce8cb71272
Alexey Brazhnik <alexey.brazhnyk@gmail.com>
2022-07-26 22:39:22 +0300

    второе задание

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const playerRef = document.querySelector('#vimeo-player');
+const player = new Vimeo(playerRef);
+
+setPlayerCurrentTime();
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function saveCurrentTime(time) {
+  localStorage.setItem('videoplayer-current-time', time.seconds);
+}
+
+function setPlayerCurrentTime() {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+
+  if (currentTime) player.setCurrentTime(currentTime);
+}
