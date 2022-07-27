ed25ac96178f15b0281846c2038c6382086a605f
Alexey Brazhnik <alexey.brazhnyk@gmail.com>
2022-07-27 06:58:48 +0300

    начало третьей работы

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,10 @@
+import throttle from 'lodash.throttle';
 
+feedbackFormRef = document.querySelector('.feedback-form');
+
+feedbackFormRef.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(e) {
+  localStorage.setItem(e.target.name, e.target.value);
+  //   console.log(e.target.name);
+}
