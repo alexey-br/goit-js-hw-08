af2613777bbc8885a2daccbd0854f030969d0b3d
Alexey Brazhnik <alexey.brazhnyk@gmail.com>
2022-07-27 22:29:00 +0300

    окончание 3го задания

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,21 +1,47 @@
 import throttle from 'lodash.throttle';
 
+const STATE_STORAGE_KEY = 'feedback-form-state';
+
 feedbackFormRef = document.querySelector('.feedback-form');
 
 feedbackFormRef.addEventListener('input', throttle(saveFormState, 500));
+feedbackFormRef.addEventListener('submit', onFormSubmit);
 
-retrieveFormState();
+retrieveFormState(feedbackFormRef);
 
-
-function retrieveFormState() {
-  if (!localStorage.getItem('feedback-form-state')) return;
-
-  const { email, message } = JSON.parse(localStorage.getItem('feedback-form-state')); 
-  feedbackFormRef.email.value = email;
-  feedbackFormRef.message.value = message;
+function saveFormState(e) {
+  const formRef = e.target.closest('form');
+  const formState = {
+    email: formRef.email.value,
+    message: formRef.message.value,
+  };
+  localStorage.setItem(STATE_STORAGE_KEY, JSON.stringify(formState));
 }
 
-function saveFormState() {
-  const formState = {email: feedbackFormRef.email.value, message: feedbackFormRef.message.value}
-  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const formState = {
+    email: e.currentTarget.email.value,
+    message: e.currentTarget.message.value,
+  };
+  console.log(formState);
+
+  e.currentTarget.reset();
+
+  clearFormState();
+}
+
+function retrieveFormState(formRef) {
+  if (!localStorage.getItem(STATE_STORAGE_KEY)) return;
+
+  const { email, message } = JSON.parse(
+    localStorage.getItem(STATE_STORAGE_KEY)
+  );
+  formRef.email.value = email;
+  formRef.message.value = message;
+}
+
+function clearFormState() {
+  localStorage.removeItem(STATE_STORAGE_KEY);
 }
