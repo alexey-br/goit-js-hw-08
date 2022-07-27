e82c29cb5b88622142cddc5cf2d6bba6fb4ca085
Alexey Brazhnik <alexey.brazhnyk@gmail.com>
2022-07-27 16:19:37 +0300

    задание 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,9 +2,20 @@ import throttle from 'lodash.throttle';
 
 feedbackFormRef = document.querySelector('.feedback-form');
 
-feedbackFormRef.addEventListener('input', throttle(onFormInput, 500));
+feedbackFormRef.addEventListener('input', throttle(saveFormState, 500));
 
-function onFormInput(e) {
-  localStorage.setItem(e.target.name, e.target.value);
-  //   console.log(e.target.name);
+retrieveFormState();
+
+
+function retrieveFormState() {
+  if (!localStorage.getItem('feedback-form-state')) return;
+
+  const { email, message } = JSON.parse(localStorage.getItem('feedback-form-state')); 
+  feedbackFormRef.email.value = email;
+  feedbackFormRef.message.value = message;
+}
+
+function saveFormState() {
+  const formState = {email: feedbackFormRef.email.value, message: feedbackFormRef.message.value}
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
 }
